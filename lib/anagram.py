b67d297e3c662e7bf9c08857e237f8839e163517
Jerricke <eejerrickw@gmail.com>
2023-07-25 16:06:05 -0600

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+
+class Anagram:
+    def __init__(self, target):
+        self.target = target
+
+    def match(self, words):
+        result = []
+        for word in words:
+            if set(word) == set(self.target):
+                result.append(word)
+            else:
+                return result
+        return result
+
+    pass
